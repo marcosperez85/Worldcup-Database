f2bb387711ab9bbdd38f0709472e3d4a7a48dd26
Marcos Perez <perezmarcos04@yahoo.com.ar>
2024-12-29 19:45:43 -0300

    Agrego script inicial pero aun no es funcional. Sólo sirve como backup para restaurar proyecto en FreeCode Camp

--- insert_data.sh
+++ insert_data.sh
@@ -8,3 +8,11 @@ else
 fi
 
 # Do not change code above this line. Use the PSQL variable above to query your database.
+
+# When you run your insert_data.sh script, it should add each unique team to the teams table. There should be 24 rows
+cat games.csv | while IFS="," READ WINNER
+do
+  if [[ $WINNER = "winner" ]]
+    INSERT_WINNER=$($PSQL "INSERT INTO teams VALUES('$WINNER')")
+  fi
+done
