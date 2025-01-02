f43df32cd8dcade0e15dde655e624ca9a7fe6803
Marcos Perez <perezmarcos04@yahoo.com.ar>
2025-01-02 13:56:35 -0300

    Agrego noveno query. Notar el uso del OR dentro del INNER JOIN ON

--- queries.sh
+++ queries.sh
@@ -29,7 +29,7 @@ echo -e "\nWinner of the 2018 tournament team name:"
 echo "$($PSQL "SELECT name FROM teams INNER JOIN games ON teams.team_id = games.winner_id WHERE games.round = 'Final' AND games.year = '2018'")"
 
 echo -e "\nList of teams who played in the 2014 'Eighth-Final' round:"
-echo
+echo "$($PSQL "SELECT name FROM teams INNER JOIN games ON teams.team_id = games.winner_id OR teams.team_id = games.opponent_id WHERE games.round = 'Eighth-Final' AND games.year = '2014' ORDER BY name")"
 
 echo -e "\nList of unique winning team names in the whole data set:"
 echo "$($PSQL "SELECT DISTINCT name FROM teams INNER JOIN games ON teams.team_id = games.winner_id WHERE games.winner_goals > games.opponent_goals ORDER BY name;")"
