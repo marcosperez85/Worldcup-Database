6a2a5cd534a6eecd2434142fd5d176c6e8dc47a9
Marcos Perez <perezmarcos04@yahoo.com.ar>
2025-01-02 09:45:41 -0300

    Completo el sexto query

--- queries.sh
+++ queries.sh
@@ -20,7 +20,7 @@ echo -e "\nAverage number of goals in all games from both teams:"
 echo "$($PSQL "SELECT AVG(winner_goals + opponent_goals) FROM games")"
 
 echo -e "\nMost goals scored in a single game by one team:"
-echo
+echo  "$($PSQL "SELECT MAX(winner_goals) FROM games")"
 
 echo -e "\nNumber of games where the winning team scored more than two goals:"
 echo
